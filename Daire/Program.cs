5bad7466026fd96d8fbc794f51bc6542721de181
Yusuf Gülmez <86079698+Developrimbor@users.noreply.github.com>
2023-09-13 16:05:02 +0300

    CSPatikaDev Circle Creator Files Upload

---
+++ Daire/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CircleShape
+{
+   class Shape
+   {
+      static void Main(string[] args)
+      {
+         int radius = 5;
+         double console_ratio = Convert.ToDouble(4.5 / 2.0);
+         double a = console_ratio * radius;
+         double b = radius;
+
+         for (int y = -radius; y <= radius; y++)
+         {
+            for(double x = -a; x <= a; x++)
+            {
+               double d = (x / a) * (x / a ) + (y / b) * (y / b);
+
+               if(d > 0.90 && d < 1.2)
+               {
+                  Console.Write("*");
+               }
+               else
+               {
+                  Console.Write(" ");
+               }
+            }
+            Console.Write("\n");
+         }
+         Console.Read();
+      }
+   }
+}
